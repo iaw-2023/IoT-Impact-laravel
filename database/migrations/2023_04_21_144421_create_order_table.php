e53f5ff02317cb3bfe3960ea0f24c45716fc173d
Matías David Schwerdt <schwerdt.matias@gmail.com>
2023-04-21 12:33:46 -0300

    Add columns to order table

--- database/migrations/2023_04_21_144421_create_order_table.php
+++ database/migrations/2023_04_21_144421_create_order_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('order', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('customer_email');
+            $table->float('total_amount');
         });
     }
 
