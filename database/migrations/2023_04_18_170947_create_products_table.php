bde4918104f82e27362eaffccf1ae719581ad64a
Matías David Schwerdt <schwerdt.matias@gmail.com>
2023-04-21 12:00:37 -0300

    Rename product table

--- database/migrations/2023_04_18_170947_create_products_table.php
+++ database/migrations/2023_04_18_170947_create_products_table.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('product', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string('nombre');
-            $table->float('precio');
+            $table->string('name');
+            $table->float('price');
             $table->integer('stock');
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('product');
+        Schema::dropIfExists('products');
     }
 };
