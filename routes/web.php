cba243c036416f98ff4a0c7328e5f4ac1e0b5655
Rodri Kraus <rodrikraus10@hotmail.com>
2023-05-09 16:02:31 -0300

    Closes issue #5 - Test

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,6 @@ Route::middleware(['auth'])->group(function () {
      Route::post('/categories/destroy', [ProductCategoryController::class, 'destroy'])->name('categories.destroy'); 
      Route::post('/categories/update', [ProductCategoryController::class, 'update'])->name('categories.update');  
 
-
 });
 
 
