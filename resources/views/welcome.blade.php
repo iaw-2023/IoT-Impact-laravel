9181119ec0410ac4fc36ca88112fa1af7297d183
Rodri Kraus <rodrikraus10@hotmail.com>
2023-05-11 20:39:32 -0300

    Test2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
             <a class="navbar-brand" href="/">BurgerPlanet</a>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item"> <a class="nav-link active" aria-current="page" href="/products">Productoss</a> </li>
+                    <li class="nav-item"> <a class="nav-link active" aria-current="page" href="/products">Productos</a> </li>
                     <li class="nav-item"> <a class="nav-link" aria-current="page" href="/categories">Categorias</a> </li>
                     <li class="nav-item"> <a class="nav-link" aria-current="page" href="/orders">Ordenes</a> </li>
                     <li class="nav-item"> <a class="nav-link" aria-current="page" href="/items">Items</a> </li>
