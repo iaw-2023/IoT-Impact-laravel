b734da142f3b9ef2b08bf9cdda0d2d8433a578ab
rodrikraus <rodrikraus10@hotmail.com>
2023-06-28 13:38:38 -0300

    minifix

--- app/Http/Controllers/UserReactController.php
+++ app/Http/Controllers/UserReactController.php
@@ -6,6 +6,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Models\UserReact;
+use Illuminate\Support\Facades\Bcrypt;
 
 class UserReactController extends Controller
 {
